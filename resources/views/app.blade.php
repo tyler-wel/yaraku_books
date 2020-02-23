d74eea9493dff4b0cdd386c42afbee432db0054c
tyler-wel <tylwelsh@gmail.com>
2020-02-23 18:23:42 +0900

    switched asset -> secured_asset for deployment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,12 +11,11 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
   </head>
   <body>
     <div id="app"></div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}"></script>
   </body>
 </html>
