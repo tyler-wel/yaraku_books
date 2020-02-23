ed905e4287faa42953ed883c36f3f888f47fb75f
tyler-wel <tylwelsh@gmail.com>
2020-02-23 18:50:06 +0900

    revert database config changes and force push database

--- config/database.php
+++ config/database.php
@@ -37,7 +37,8 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => dirname(__DIR__).'/database/database.sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
